d65ad341e57b5d6f742941b722929be3df271c68
Serhii Litvinenko <slitvinenko@gbsfo.com>
2019-09-25 18:26:15 +0300

    1.1.2

--- feedback-feedback/src/main/java/com/core/feedback/FeedbackRatingDialog.kt
+++ feedback-feedback/src/main/java/com/core/feedback/FeedbackRatingDialog.kt
@@ -3,11 +3,17 @@ package com.core.feedback
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RatingBar
 import androidx.fragment.app.DialogFragment
+import com.hanks.lineheightedittext.LineHeightEditText
 import kotlinx.android.synthetic.main.dialog_feedback_partial_buttons.view.*
 import kotlinx.android.synthetic.main.dialog_feedback_rating.view.*
 
@@ -21,6 +27,10 @@ class FeedbackRatingDialog(
     var onOkBtnClickAction: (rating: Int, review: String) -> Unit,
     var closeBtnText: String?
 ) : DialogFragment() {
+
+    lateinit var reviewET: LineHeightEditText
+    lateinit var ratingRB: RatingBar
+    lateinit var sendBTN: Button
 
     override fun onStart() {
         super.onStart()
@@ -39,11 +49,30 @@ class FeedbackRatingDialog(
 
         val view = inflater.inflate(R.layout.dialog_feedback_rating, container, false)
 
+        reviewET = view.dialog_fragment_feedback_et_review
+        ratingRB = view.dialog_fragment_feedback_ratingbar
+        sendBTN = view.dialog_fragment_feedback_button_send
+
         view.dialog_fragment_feedback_title.text = title
-        view.dialog_fragment_feedback_et_review.hint = reviewHint
+        reviewET.hint = reviewHint
         view.dialog_fragment_feedback_tv_rating_hint.text = ratingHint
-        view.dialog_fragment_feedback_button_send.text = okBtnText
+        sendBTN.text = okBtnText
         view.dialog_fragment_feedback_button_cancel.text = closeBtnText
+
+        ratingRB.onRatingBarChangeListener =
+            RatingBar.OnRatingBarChangeListener { _, _, _ -> fieldChanged() }
+        view.dialog_fragment_feedback_et_review.addTextWatcher(object : com.hanks.lineheightedittext.TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                fieldChanged()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
 
         view.dialog_fragment_feedback_button_cancel.setOnOneClickListener { dismiss() }
         view.dialog_fragment_feedback_button_send.setOnOneClickListener {
@@ -56,5 +85,10 @@ class FeedbackRatingDialog(
 
         return view
 
+    }
+
+    private fun fieldChanged() {
+        // Validate Send Button
+        sendBTN.isEnabled = ratingRB.rating > 0 && reviewET.text?.isNotEmpty() ?: false
     }
 }
